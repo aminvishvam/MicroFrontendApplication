970886aa7d980f96bcce33c6bc061c0b3caef5b1
Vishvam Amin <32908564+aminvishvam@users.noreply.github.com>
2024-03-19 14:51:41 -0400

    Adding this change using the VSC online

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,5 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const cartText = `<div>You have ${faker.random.number()} items in your cart this commit from the VSC online</div>`;
 
 document.querySelector('#cart-dev').innerHTML = cartText;
